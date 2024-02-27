ec6e2dab7d775c5c2554348d2f2760faaf82c18d
Atharv <atharve.bute@gmail.com>
2024-02-27 15:34:34 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter, Route, Routes,Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RedirectPage from "./RedirectPage";
 // import Server from "./Testing";
 import QRCode from 'qrcode.react';
